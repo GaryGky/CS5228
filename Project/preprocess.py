d04239c75ab57e5f12937193c19ca1df10d08fae
SKYSCRAPERS1999 <386333667@qq.com>
2022-10-15 23:28:27 +0800

    do preprocessing for available_unit_types, property_type, tenure, but not filtering outliers

--- Project/preprocess.py
+++ Project/preprocess.py
@@ -24,3 +24,25 @@ def preprocess_tenure(s: str):
         return "freehold"
     return "other"
     
+def parse_available_unit_types(s: str):
+    x = s.split(',')
+    mi, mx = 1000, -1000
+    for i in range(len(x)):
+        if 'studio' in x[i]:
+            continue
+        if 'br' in x[i]:
+            x[i] = x[i].split(' ')[-2]
+        try:
+            mi = min(mi, int(x[i]))
+            mx = max(mx, int(x[i]))
+        except:
+            print(x)
+    return {'type': 'studio' if 'studio' in x[0] else 'other', 'min': mi, 'max': mx}
+
+def filter_outlier_price_per_sqft(data):
+    data['price_per_sqft'] = data['price'] / data['size_sqft']
+    print("before filtering: ", data.price_per_sqft.describe())
+    data = data[(data['price_per_sqft'] <= data["price_per_sqft"].quantile(1 - 0.005)) & (data['price_per_sqft'] >= data["price_per_sqft"].quantile(0.005))]
+    print("after filtering: ", data.price_per_sqft.describe())
+    data = data.drop(['price_per_sqft'], axis=1)
+    return data
